e8cd96f5c15fc4f9bb72fe53e3a5b2afc6aad709
restgoss <restgoss420@yandex.ru>
2023-03-17 21:31:43 +0300

    ready for first review

---
+++ script.js
@@ -0,0 +1,41 @@
+let formElement = document.querySelector('.edit-form');
+let nameInput = document.querySelector('#name')
+let descriptionInput = document.querySelector('#description');
+let currentName = document.querySelector('.profile__info__name');
+let currentDescription = document.querySelector('.profile__info__description');
+
+let editButton = document.querySelector('.profile__info__edit-btn');
+let closeEditButton = document.querySelector('.edit-form__close-button');
+let saveButton = document.querySelector('.edit-form__button');
+// let heartButton = document.querySelectorAll('.element__button');
+
+let heartButton = document.querySelectorAll('.element__button').forEach(function (heartButton) {
+    heartButton.addEventListener('click', function () {
+            heartButton.classList.toggle('element__button_active');
+        })
+    });
+
+
+console.log(currentName.textContent);
+
+function popupOpen() {
+    formElement.classList.add('edit-form_active');
+}
+
+function popupClose() {
+    formElement.classList.remove('edit-form_active');
+}
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    currentName.textContent = nameInput.value;
+    currentDescription.textContent = descriptionInput.value;
+    popupClose();
+}
+
+
+formElement.addEventListener('submit', handleFormSubmit);
+saveButton.addEventListener('click', handleFormSubmit);
+closeEditButton.addEventListener('click', popupClose);
+editButton.addEventListener('click', popupOpen);
+
